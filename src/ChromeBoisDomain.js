1528030eb471b0d84d1998c4077160f8036af818
Loae Monla Ahmed <lueyahmad@gmail.com>
2020-10-30 12:42:50 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -24,6 +24,8 @@ export default class ChromeBoisDomain extends Component {
   /* if the key pressed was 's', then it should call `resize` with '-' 
    */
   
+  drawChromeBoiAtCoords(event.clientX, event.clientY);
+  
   render() {
     return (
       <canvas 
